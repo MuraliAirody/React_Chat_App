8e400c6baf40d049a6346ff8c0dd7ffccd6ecd7d
MuraliAirody <muraliairody@gmail.com>
2024-04-22 22:09:28 +0530

    user info and chat list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+
 function App() {
   return (
     <>
       <div className="container">
-        
+        <List/>
+        <Chat/>
+        <Detail/>
       </div>
     </>
   );
