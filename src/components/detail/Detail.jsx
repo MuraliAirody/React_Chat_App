e33e4c0d1bb9d1ff2ccc6b9ae75ac46e09e2b423
MuraliAirody <muraliairody@gmail.com>
2024-04-27 23:51:03 +0530

    zustand

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,6 +1,12 @@
+import { auth } from "../../lib/firebase";
 import "./detail.css";
 
 const Detail = () => {
+
+  const handleLogout = () => {
+    auth.signOut();
+  };
+
   return (
     <div className="detail">
       <div className="user">
@@ -82,7 +88,7 @@ const Detail = () => {
           </div>
         </div>
         <button>Block User</button>
-        <button className="logout">Logout</button>
+        <button className="logout" onClick={handleLogout}>Logout</button>
       </div>
     </div>
   );
